c6fedb56d9f912499fa018567cf95d3abcf928e8
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-26 14:24:05 -0600

    Fix transform function to prepend 'done - ' and recurse

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
+
 function markAsDone(todos) {
     return transform(0, todos, []);
 }
 
 function transform(index, todos, modifiedTodos){
     if(index < todos.length){
-       
-    modifiedTodos.push("done - " + todos[index]);
-  return transform(index + 1, todos, modifiedTodos;
-}
-        // on this line - prepend the string 'done - ' to each `todo` description
-        // on this line - call transform recursively
-    }else{
+        modifiedTodos.push("done - " + todos[index]);   // prepend the string
+        return transform(index + 1, todos, modifiedTodos); // recursive call
+    } else {
         return modifiedTodos;
     }
 }
 
-
-
 //should modify line 8 to prepend the string 'done - ' to each todo description
 
 //should modify line 9 to recursively call the transform function
+
 
 
 
